f8e9b588962938586e36c12be26df76291d3e017
falesi mbewe <falesiembewe@gmail.com>
2021-02-11 21:58:40 +0200

    new changes to UI

--- view/humanresource/index.php
+++ view/humanresource/index.php
@@ -124,8 +124,8 @@ require 'header.php';
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                         <div class="analytics-sparkle-line reso-mg-b-30">
                             <div class="analytics-content">
-                                <h5>Computer Technologies</h5>
-                                <h2>$<span class="counter">5000</span> <span class="tuition-fees">Tuition Fees</span></h2>
+                                <h5>Total Salary</h5>
+                                <h2>k<span class="counter">50000</span> <span class="tuition-fees"></span></h2>
                                 <span class="text-success">20%</span>
                                 <div class="progress m-b-0">
                                     <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:20%;"> <span class="sr-only">20% Complete</span> </div>
@@ -136,8 +136,8 @@ require 'header.php';
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                         <div class="analytics-sparkle-line reso-mg-b-30">
                             <div class="analytics-content">
-                                <h5>Accounting Technologies</h5>
-                                <h2>$<span class="counter">3000</span> <span class="tuition-fees">Tuition Fees</span></h2>
+                                <h5>Avg Salary</h5>
+                                <h2>k<span class="counter">7000</span> <span class="tuition-fees">Tuition Fees</span></h2>
                                 <span class="text-danger">30%</span>
                                 <div class="progress m-b-0">
                                     <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:30%;"> <span class="sr-only">230% Complete</span> </div>
@@ -148,8 +148,8 @@ require 'header.php';
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                         <div class="analytics-sparkle-line reso-mg-b-30 table-mg-t-pro dk-res-t-pro-30">
                             <div class="analytics-content">
-                                <h5>Electrical Engineering</h5>
-                                <h2>$<span class="counter">2000</span> <span class="tuition-fees">Tuition Fees</span></h2>
+                                <h5>No of Employees</h5>
+                                <h2><span class="counter">200</span> <span class="tuition-fees">Tuition Fees</span></h2>
                                 <span class="text-info">60%</span>
                                 <div class="progress m-b-0">
                                     <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:60%;"> <span class="sr-only">20% Complete</span> </div>
@@ -160,8 +160,8 @@ require 'header.php';
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                         <div class="analytics-sparkle-line table-mg-t-pro dk-res-t-pro-30">
                             <div class="analytics-content">
-                                <h5>Chemical Engineering</h5>
-                                <h2>$<span class="counter">3500</span> <span class="tuition-fees">Tuition Fees</span></h2>
+                                <h5>Employee with disability</h5>
+                                <h2><span class="counter">60</span> <span class="tuition-fees">Tuition Fees</span></h2>
                                 <span class="text-inverse">80%</span>
                                 <div class="progress m-b-0">
                                     <div class="progress-bar progress-bar-inverse" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:80%;"> <span class="sr-only">230% Complete</span> </div>
@@ -181,25 +181,25 @@ require 'header.php';
                                 <div class="row">
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                         <div class="caption pro-sl-hd">
-                                            <span class="caption-subject"><b>University Earnings</b></span>
+                                            <span class="caption-subject"><b>Professors Summary</b></span>
                                         </div>
                                     </div>
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                         <div class="actions graph-rp graph-rp-dl">
-                                            <p>All Earnings are in million $</p>
+                                            <p></p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <ul class="list-inline cus-product-sl-rp">
                                 <li>
-                                    <h5><i class="fa fa-circle" style="color: #006DF0;"></i>CSE</h5>
+                                    <h5><i class="fa fa-circle" style="color: #006DF0;"></i>Turnover rate</h5>
                                 </li>
                                 <li>
-                                    <h5><i class="fa fa-circle" style="color: #933EC5;"></i>Accounting</h5>
+                                    <h5><i class="fa fa-circle" style="color: #933EC5;"></i>Professor Absenteesism Rate</h5>
                                 </li>
                                 <li>
-                                    <h5><i class="fa fa-circle" style="color: #65b12d;"></i>Electrical</h5>
+                                    <h5><i class="fa fa-circle" style="color: #65b12d;"></i>Overall Performance</h5>
                                 </li>
                             </ul>
                             <div id="extra-area-chart" style="height: 356px;"></div>
@@ -207,46 +207,46 @@ require 'header.php';
                     </div>
                     <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                         <div class="white-box analytics-info-cs mg-b-10 res-mg-b-30 res-mg-t-30 table-mg-t-pro-n tb-sm-res-d-n dk-res-t-d-n">
-                            <h3 class="box-title">Total Visit</h3>
+                            <h3 class="box-title">Total No of Students</h3>
                             <ul class="list-inline two-part-sp">
                                 <li>
                                     <div id="sparklinedash"></div>
                                 </li>
-                                <li class="text-right sp-cn-r"><i class="fa fa-level-up" aria-hidden="true"></i> <span class="counter text-success">1500</span></li>
+                                <li class="text-right sp-cn-r"><i class="fa fa-level-up" aria-hidden="true"></i> <span class="counter text-success">500</span></li>
                             </ul>
                         </div>
                         <div class="white-box analytics-info-cs mg-b-10 res-mg-b-30 tb-sm-res-d-n dk-res-t-d-n">
-                            <h3 class="box-title">Page Views</h3>
+                            <h3 class="box-title">Students with Disability</h3>
                             <ul class="list-inline two-part-sp">
                                 <li>
                                     <div id="sparklinedash2"></div>
                                 </li>
-                                <li class="text-right graph-two-ctn"><i class="fa fa-level-up" aria-hidden="true"></i> <span class="counter text-purple">3000</span></li>
+                                <li class="text-right graph-two-ctn"><i class="fa fa-level-up" aria-hidden="true"></i> <span class="counter text-purple">150</span></li>
                             </ul>
                         </div>
                         <div class="white-box analytics-info-cs mg-b-10 res-mg-b-30 tb-sm-res-d-n dk-res-t-d-n">
-                            <h3 class="box-title">Unique Visitor</h3>
+                            <h3 class="box-title">Total No of Professors</h3>
                             <ul class="list-inline two-part-sp">
                                 <li>
                                     <div id="sparklinedash3"></div>
                                 </li>
-                                <li class="text-right graph-three-ctn"><i class="fa fa-level-up" aria-hidden="true"></i> <span class="counter text-info">5000</span></li>
+                                <li class="text-right graph-three-ctn"><i class="fa fa-level-up" aria-hidden="true"></i> <span class="counter text-info">50</span></li>
                             </ul>
                         </div>
                         <div class="white-box analytics-info-cs table-dis-n-pro tb-sm-res-d-n dk-res-t-d-n">
-                            <h3 class="box-title">Bounce Rate</h3>
+                            <h3 class="box-title">Professors With Disability</h3>
                             <ul class="list-inline two-part-sp">
                                 <li>
                                     <div id="sparklinedash4"></div>
                                 </li>
-                                <li class="text-right graph-four-ctn"><i class="fa fa-level-down" aria-hidden="true"></i> <span class="text-danger"><span class="counter">18</span>%</span>
+                                <li class="text-right graph-four-ctn"><i class="fa fa-level-down" aria-hidden="true"></i> <span class="text-danger"><span class="counter">18</span></span>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> <!--
         <div class="traffic-analysis-area">
             <div class="container-fluid">
                 <div class="row">
@@ -445,7 +445,7 @@ require 'header.php';
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <div class="product-sales-area mg-tb-30">
             <div class="container-fluid">
                 <div class="row">
@@ -455,7 +455,7 @@ require 'header.php';
                                 <div class="row">
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                         <div class="caption pro-sl-hd">
-                                            <span class="caption-subject"><b>Adminsion Statistic</b></span>
+                                            <span class="caption-subject"><b>Student Summary</b></span>
                                         </div>
                                     </div>
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
@@ -472,13 +472,13 @@ require 'header.php';
                             </div>
                             <ul class="list-inline cus-product-sl-rp">
                                 <li>
-                                    <h5><i class="fa fa-circle" style="color: #006DF0;"></i>Python</h5>
+                                    <h5><i class="fa fa-circle" style="color: #006DF0;"></i>Student Absenteesism Rate</h5>
                                 </li>
                                 <li>
-                                    <h5><i class="fa fa-circle" style="color: #933EC5;"></i>PHP</h5>
+                                    <h5><i class="fa fa-circle" style="color: #933EC5;"></i>Students Overall Performance</h5>
                                 </li>
                                 <li>
-                                    <h5><i class="fa fa-circle" style="color: #65b12d;"></i>Java</h5>
+                                    <h5><i class="fa fa-circle" style="color: #65b12d;"></i>Student Participation</h5>
                                 </li>
                             </ul>
                             <div id="morris-area-chart"></div>
@@ -487,56 +487,56 @@ require 'header.php';
                     <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                         <div class="analysis-progrebar res-mg-t-30 mg-ub-10 res-mg-b-30 table-mg-t-pro-n tb-sm-res-d-n dk-res-t-d-n">
                             <div class="analysis-progrebar-content">
-                                <h5>Usage</h5>
-                                <h2 class="storage-right"><span class="counter">90</span>%</h2>
+                                <h5>Learning Hours</h5>
+                                <h2 class="storage-right"><span class="counter">600</span>hrs</h2>
                                 <div class="progress progress-mini ug-1">
                                     <div style="width: 68%;" class="progress-bar"></div>
                                 </div>
                                 <div class="m-t-sm small">
-                                    <p>Server down since 1:32 pm.</p>
+                                    <p>Learning Hours Per Semester</p>
                                 </div>
                             </div>
                         </div>
                         <div class="analysis-progrebar reso-mg-b-30 res-mg-b-30 mg-ub-10 tb-sm-res-d-n dk-res-t-d-n">
                             <div class="analysis-progrebar-content">
-                                <h5>Memory</h5>
-                                <h2 class="storage-right"><span class="counter">70</span>%</h2>
+                                <h5>Avg Learning Hours</h5>
+                                <h2 class="storage-right"><span class="counter">5</span>hrs</h2>
                                 <div class="progress progress-mini ug-2">
                                     <div style="width: 78%;" class="progress-bar"></div>
                                 </div>
                                 <div class="m-t-sm small">
-                                    <p>Server down since 12:32 pm.</p>
+                                    <p>Learning Hours Per Day.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="analysis-progrebar reso-mg-b-30 res-mg-b-30 res-mg-t-30 mg-ub-10 tb-sm-res-d-n dk-res-t-d-n">
                             <div class="analysis-progrebar-content">
-                                <h5>Data</h5>
-                                <h2 class="storage-right"><span class="counter">50</span>%</h2>
+                                <h5>CA Performance</h5>
+                                <h2 class="storage-right"><span class="counter">70</span>%</h2>
                                 <div class="progress progress-mini ug-3">
                                     <div style="width: 38%;" class="progress-bar progress-bar-danger"></div>
                                 </div>
                                 <div class="m-t-sm small">
-                                    <p>Server down since 8:32 pm.</p>
+                                    <p>Performance By All Students</p>
                                 </div>
                             </div>
                         </div>
                         <div class="analysis-progrebar res-mg-t-30 table-dis-n-pro tb-sm-res-d-n dk-res-t-d-n">
                             <div class="analysis-progrebar-content">
-                                <h5>Space</h5>
-                                <h2 class="storage-right"><span class="counter">40</span>%</h2>
+                                <h5>Examinations Performance</h5>
+                                <h2 class="storage-right"><span class="counter">80</span>%</h2>
                                 <div class="progress progress-mini ug-4">
                                     <div style="width: 28%;" class="progress-bar progress-bar-danger"></div>
                                 </div>
                                 <div class="m-t-sm small">
-                                    <p>Server down since 5:32 pm.</p>
+                                    <p>Performance By All Students</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> <!--
         <div class="courses-area mg-b-15">
             <div class="container-fluid">
                 <div class="row">
@@ -619,7 +619,7 @@ require 'header.php';
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <div class="footer-copyright-area">
             <div class="container-fluid">
                 <div class="row">
